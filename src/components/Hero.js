85f96305ffbc79c4fad63da5dbbbae9996a8ce4c
suresh-qloop <sureshdankhara@gmail.com>
2024-06-27 10:16:03 +0530

    Add animation in hero page

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-sync-scripts */
 import React from 'react'
 import { Carousel } from 'antd'
 
@@ -5,11 +6,17 @@ const Hero = () => {
   return (
     <div className='bg-[#F2F2F2] xl:w-full px-[40px] xl:px-0'>
       <div className='flex flex-col justify-center items-center Matter container mx-auto pb-[20px] sm:pb-[100px] pt-[60px] sm:pt-0 sm:pr-[50px]'>
-        <div className='flex flex-col items-center sm:flex-row sm:justify-between lg:mx-[4%] xl:mx-0 2xl:mx-[9%] sm:gap-[20px] xl:gap-[230px] 2xl:gap-[260px]'>
-            <div>
+        <div className='flex flex-col max-sm:items-center sm:flex-row sm:justify-between lg:mx-[4%] xl:mx-0 2xl:mx-[9%] sm:gap-[20px] xl:gap-[230px] 2xl:gap-[260px]'>
+            <div className='sm:flex sm:flex-col'>
+                <div className='mb-[27px] lg:mb-[30px] xl:mb-[35px] hidden sm:block'>
+                    <script src="https://unpkg.com/@dotlottie/player-component@latest/dist/dotlottie-player.mjs" type="module"></script>
+                    <div className="w-[300px] lg:w-[370px] xl:w-[400px] h-[300px] lg:h-[370px] xl:h-[400px]">
+                    <dotlottie-player src="https://lottie.host/1fee5ab9-21d0-4152-88dc-b6a54535a0f8/sKPpPFJnMD.json" background="transparent" speed="1" loop autoplay></dotlottie-player>
+                    </div>
+                </div>
                 <div className='flex flex-col' style={{transform: "translate(0px, 0px)", opacity: "1"}}>
-                    <div className='text-[30px] lg:text-[45px] font-bold sm:text-left text-center ubuntu-bold'>
-                        Power&#39;d by <span className="text-[30px] lg:text-[45px] font-bold bg-gradient-to-r from-green-500 via-orange-500 to-red-500 bg-clip-text text-transparent ubuntu-bold">$CRT</span> Token <br /><span className="text-[30px] lg:text-[45px] font-bold text-[#0946F6] ubuntu-bold">Qtum</span> DeFi.
+                    <div className='text-[30px] lg:text-[44px] xl:text-[45px] font-bold sm:text-left text-center ubuntu-bold'>
+                        Power&#39;d by <span className="text-[30px] lg:text-[44px] xl:text-[45px] font-bold bg-gradient-to-r from-green-500 via-orange-500 to-red-500 bg-clip-text text-transparent ubuntu-bold">$CRT</span> Token <br /><span className="text-[30px] lg:text-[44px] xl:text-[45px] font-bold text-[#0946F6] ubuntu-bold">Qtum</span> DeFi.
                     </div>
                     <a href="#" className="w-[160px] lg:w-[218px] mt-[30px] lg:mt-[40px]"><div className="hidden sm:flex cursor-pointer bg-[#ff914d] rounded-full w-[160px] lg:w-[218px] py-3 lg:py-4 justify-center font-semibold text-[14px] lg:text-[20px]" style={{transform: "none"}}>Download App</div></a>
                 </div>
